90c83ccf6cc8ae2c609f576f7bccf32c86e01b19
Mounika1695 <mounika.mouni1695@gmail.com>
2022-04-11 13:52:48 +0530

    4 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GaugeModule } from 'angular-gauge/gauge.module';
+import { MatFormFieldModule } from '@angular/material/form-field/form-field-module';
+import { MatSelectModule } from '@angular/material/select/select-module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    GaugeModule.forRoot(),
+    MatFormFieldModule,
+    MatSelectModule,
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
